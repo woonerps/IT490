8ba4856f33acd1942c74ba80e520c008f3a0ef00
Woonerps <pw49@njit.edu>
2018-03-03 17:32:06 -0500

    First commit

---
+++ steam_Peter.js
@@ -0,0 +1,24 @@
+var jcontent = {
+
+		
+				"steamid": "76561198065539419",
+				"communityvisibilitystate": 3,
+				"profilestate": 1,
+				"personaname": "(#)Lewd",
+				"lastlogoff": 1520107033,
+				"profileurl": "http://steamcommunity.com/profiles/76561198065539419/",
+				"avatar": "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/b6/b6a7e68aff8dc582aed756b331e6185b77ad583b.jpg",
+				"avatarmedium": "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/b6/b6a7e68aff8dc582aed756b331e6185b77ad583b_medium.jpg",
+				"avatarfull": "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/b6/b6a7e68aff8dc582aed756b331e6185b77ad583b_full.jpg",
+				"personastate": 1,
+				"realname": "Peter",
+				"primaryclanid": "103582791435167561",
+				"timecreated": 1340041341,
+				"personastateflags": 0
+			
+		
+		
+	}
+var output = document.getElementById('output');
+
+output.innerHTML= jcontent.personaname;
